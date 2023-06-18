cdb695725e74a1dd81a08c960add16aaabc433a2
naaaaaaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-06-18 09:17:06 +0000

    start

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: "homes#top"
+  devise_for :users
+  resourcers :books[:new, :index, :show, :edit]
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
